7bab30099118e585f971c0b6029fde40e48963b7
Shubham <lekhak.sub@gmail.com>
2023-06-05 00:28:12 +0545

    Installed pod, Set up firebase & Email/Pass login

--- Messenger/Controllers/Login/LoginViewController.swift
+++ Messenger/Controllers/Login/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
     
@@ -135,6 +136,14 @@ class LoginViewController: UIViewController {
             return
         }
 //        Firebase login
+        FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password, completion: { authResult, error in
+            guard let result = authResult, error == nil else {
+                print("Failed to log in user with email: \(email)")
+                return
+            }
+            let user = result.user
+            print("Logged In User: \(user)")
+        })
         
     }
     
